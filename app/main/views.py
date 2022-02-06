0df34091f27cace58c9e6d67e9271ba21cef6ed5
Francis Githae <francis.githae@student.moringaschool.com>
2022-02-06 19:19:44 +0300

    display pitch form

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,15 @@
 from flask import render_template
 from . import main
-
+from .forms import PitchForm
 
 @main.route('/')
 def index():
-  return render_template('index.html')
+  pitch_form=PitchForm()
+
+  # if pitch_form.validate_on_submit():
+  #   new_p
+
+  return render_template('index.html',pitch_form=pitch_form)
 
 @main.route('/user/<user_name>')
 def profile(user_name):
