d21c796cd5ede9b32deaecfcb36d560e1a956b48
Francis Githae <francis.githae@student.moringaschool.com>
2022-02-06 17:04:33 +0300

    add user registration view function

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,13 +1,19 @@
-from flask import render_template
+from flask import render_template,redirect,url_for,flash,request
 from . import auth
 from .forms import RegistrationForm,LoginForm
-
+from .. import db
+from flask_login import login_user,logout_user,login_required
+from ..models import User,Role
 @auth.route('/login')
 def login():
   login_form=LoginForm()
   return render_template('auth/login.html',login_form=login_form)
 
-@auth.route('/register')
+@auth.route('/register',methods=["GET","POST"])
 def register():
   register_form=RegistrationForm()
+  if register_form.validate_on_submit():
+    user=User(name=register_form.name.data,email=register_form.email.data,username=register_form.username.data,password=register_form.password.data,role=Role.query.filter_by(name='User').first())
+    user.save_user()
+    return redirect(url_for('auth.login'))
   return render_template('auth/register.html',register_form=register_form)
--- app/models.py
+++ app/models.py
@@ -1,8 +1,14 @@
 
-from .import db
+from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from datetime import datetime
+from . import login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+  return User.query.get(int(user_id))
+
 class User(UserMixin,db.Model):
   '''
   User class that defines user objects
