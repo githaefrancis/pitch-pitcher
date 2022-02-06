a515deaad38aacfa4279c660a806ddb18e998227
Francis Githae <francis.githae@student.moringaschool.com>
2022-02-06 20:57:04 +0300

    implement submitting and saving pitches

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,10 +1,10 @@
-from unicodedata import category
+
 from flask_wtf import FlaskForm
 from wtforms import TextAreaField,SelectField,SubmitField
 
 from wtforms.validators import InputRequired
 
 class PitchForm(FlaskForm):
-  pitch=TextAreaField('',validators=[InputRequired()],render_kw={"placeholder": "Pitch away.."})
+  content=TextAreaField('Pitch',validators=[],render_kw={"placeholder": "Pitch away.."})
   category=SelectField('Category',choices=[('pickup_lines','Pickup Line'),('product_pitch','Product Pitch'),('promotional_pitch','Promotional Pitch'),('interview','Interview')],validators=[InputRequired()])
   submit=SubmitField('Submit')
--- app/main/views.py
+++ app/main/views.py
@@ -1,16 +1,15 @@
-from unicodedata import category
+
 from flask import redirect, render_template, url_for
-from flask_login import current_user
+from flask_login import current_user, login_required
 from . import main
 from .forms import PitchForm
 from ..models import Category, Pitch
 
-@main.route('/')
+@main.route('/',methods=['GET'])
 def index():
   pitch_form=PitchForm()
 
-  if pitch_form.validate_on_submit():
-    redirect(url_for('main.post',pitch_form=pitch_form))
+
 
   return render_template('index.html',pitch_form=pitch_form)
 
@@ -18,6 +17,13 @@ def index():
 def profile(user_name):
   return '<h1> Hello</h1>'
 
-@main.route('/post/new/<pitch_form>',methods=['GET','POST'])
-def post(pitch_form):
-  new_pitch=Pitch(category=Category.query.filter_by(name=pitch_form.category.data),content=pitch_form.content.data,user=current_user)
+
+@main.route('/',methods=['POST'])
+@login_required
+def post():
+  pitch_form=PitchForm()
+
+  if pitch_form.validate_on_submit():
+    new_pitch=Pitch(category=Category.query.filter_by(name=pitch_form.category.data).first(),content=pitch_form.content.data,user=current_user)
+    new_pitch.save_pitch()
+    return redirect(url_for('main.index'))
