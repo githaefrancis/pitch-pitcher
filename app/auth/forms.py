4d6fff739f8fa75ec0f86b118072eb464a4f184f
Francis Githae <francis.githae@student.moringaschool.com>
2022-02-06 15:29:05 +0300

    display register form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -23,7 +23,7 @@ class RegistrationForm(FlaskForm):
       raise ValidationError('There is an account with that email')
 
   def validate_username(self,data_field):
-    if User.quey.filter_by(email=data_field.data).first():
+    if User.query.filter_by(email=data_field.data).first():
       raise ValidationError('That username is taken')
 
 class LoginForm(FlaskForm):
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,12 @@
+from flask import render_template
 from . import auth
-
+from .forms import RegistrationForm,LoginForm
 
 @auth.route('/login')
 def login():
   return '<h1>Login page</h1>'
+
+@auth.route('/register')
+def register():
+  register_form=RegistrationForm()
+  return render_template('auth/register.html',register_form=register_form)
