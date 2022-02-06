4300f055dfdc361dd463f2e7ff29ee7ac3f4d4d1
Francis Githae <francis.githae@student.moringaschool.com>
2022-02-06 17:21:06 +0300

    add login

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,9 +4,16 @@ from .forms import RegistrationForm,LoginForm
 from .. import db
 from flask_login import login_user,logout_user,login_required
 from ..models import User,Role
-@auth.route('/login')
+@auth.route('/login',methods=['GET','POST'])
 def login():
   login_form=LoginForm()
+  if login_form.validate_on_submit():
+    user=User.query.filter_by(email=login_form.email.data).first()
+    if user is not None and user.verify_password(login_form.password.data):
+      login_user(user,login_form.remember)
+      return redirect(request.args.get('next') or url_for('main.index'))
+    flash('Invalid username or password')
+  title="Pitch Pitcher Login"
   return render_template('auth/login.html',login_form=login_form)
 
 @auth.route('/register',methods=["GET","POST"])
